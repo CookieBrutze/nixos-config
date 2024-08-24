156c0062222379c666a8ea097415abfb94ea6dcd
CookieBrutze <davidkitzelmann8@proton.me>
2024-08-24 16:48:38 +0200

    Added missing import paths

--- configuration.nix
+++ configuration.nix
@@ -7,5 +7,10 @@
         ./modules/base.nix
         ./modules/networking.nix
         ./modules/user.nix
+        ./modules/apps/dev.nix
+        ./modules/apps/firefox.nix
+        ./modules/apps/hyprland.nix
+        ./modules/apps/shell.nix
+        ./modules/apps/steam.nix
     ];
 }
