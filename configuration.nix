565c534d71c568deeae424d9ba20556488f5fdbd
CookieBrutze <davidkitzelmann8@proton.me>
2024-08-30 22:13:52 +0200

    send help

--- configuration.nix
+++ configuration.nix
@@ -4,13 +4,15 @@
     imports =
     [
         ./hardware-configuration.nix
+
         ./modules/base.nix
         ./modules/networking.nix
         ./modules/user.nix
+
         ./modules/apps/dev.nix
         ./modules/apps/firefox.nix
         ./modules/apps/hyprland.nix
-        ./modules/apps/shell.nix
+        ./modules/apps/fish.nix
         ./modules/apps/steam.nix
         ./modules/apps/gnome.nix
     ];
